4e9243ff74bae9633e2fc6e3dce9e1e5dca282c4
ryan001110 <168542203+ryan001110@users.noreply.github.com>
2025-08-25 10:50:16 -0300

    criei as listas

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-conviadados_da_festa = ['ana' , 'diego']
+# Lista de convidados para a festa 
+conviadados_da_festa = ['ana' , 'diego' ,' 'yuliany']
+
+ status_presenca = {}
+
+#lista de pessoas de tentaram entrar
+lista_de_chegadas = ['ana', 'diego']
+
